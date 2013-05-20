5cbc288a401a5254decc946d3957060b78034562
Lloyd Meta <lloydmeta@gmail.com>
2013-05-20 21:09:25 +0900

    Reverse the List

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -4,10 +4,10 @@ import collection.immutable._
 class NodeContainer(node: Node) {
 
   private var nodes = List(node)
-  var working_node = node.getNext()
+  var working_node = node.getNext
   while (working_node != null){
-    nodes = working_node :: nodes
-    working_node =  working_node.getNext()
+    nodes = nodes ::: List(working_node)
+    working_node =  working_node.getNext
   }
 
   def list: List[Node] = nodes
@@ -19,17 +19,10 @@ object TestScalaMecab extends App {
   override def main(args: Array[String]) {
     System.loadLibrary("MeCab")
 
-    var tagger = new Tagger()
+    var tagger = new Tagger
 
     val str = "まず、僕のsbt のバージョンはくず"
     println(tagger.parse(str));
-    // var node = tagger.parseToNode(str)
-
-    // while(node != null){
-    //   println(node.getSurface() + "\t" + node.getFeature());
-    //   node = node.getNext();
-    // }
-
 
     val node = tagger.parseToNode(str);
     val container = new NodeContainer(node)
