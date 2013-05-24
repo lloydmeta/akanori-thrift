5c7c73b6ce3252a317508c3b54d1f13b79b37d7f
Lloyd Meta <lloydmeta@gmail.com>
2013-05-24 09:10:05 +0900

    Stash commit

--- src/main/scala/org/beachape/actors/FileToRedisActor.scala
+++ src/main/scala/org/beachape/actors/FileToRedisActor.scala
@@ -3,6 +3,10 @@ import akka.actor.Actor
 import akka.event.Logging
 import com.redis._
 import org.beachape.analyze.FileMorphemesToRedis
+import akka.actor.Props
+import akka.pattern.ask
+import akka.routing.RoundRobinRouter
+import scala.io.Source
 import scala.concurrent.{Await, Future}
 import akka.util.Timeout
 import scala.concurrent.duration._
@@ -11,22 +15,31 @@ import scala.concurrent.duration._
 class FileToRedisActor(redisPool: RedisClientPool, dropBlacklisted: Boolean, onlyWhitelisted: Boolean) extends Actor {
 
   import context.dispatcher
+  implicit val timeout = Timeout(600 seconds)
+
+  val morphemeAnalyzerRoundRobin = context.actorOf(Props(new MorphemesAnalyzerActor(redisPool)).withRouter(RoundRobinRouter(4)), "morphemesAnalyzerRoundRobin")
 
   def receive = {
 
     case FilePath(filePath) => {
-      sender ! RedisKey(dumpFileToRedis(filePath))
+      val redisKey = redisKeyForPath(filePath)
+
+      val listOfAnalyzeAndDumpFutures = Source.fromFile(filePath).getLines().flatMap(line =>
+        ask(morphemeAnalyzerRoundRobin, List('dumpMorphemesToRedis, redisKey, line, dropBlacklisted, onlyWhitelisted))
+      ).toList
+
+      val futureListOfAnalyzeAndDumpResults = Future.sequence(listOfAnalyzeAndDumpFutures)
+
+      futureListOfAnalyzeAndDumpResults map {analyzeAndDumpResultsList =>
+        analyzeAndDumpResultsList match {
+          case x:List[Boolean] if x.forall(_ == true) => sender ! RedisKey(redisKey)
+          case _ => exit(0)
+        }
+      }
     }
 
     case _ => println("huh?")
   }
 
   def redisKeyForPath(path: String) = f"trends:$path%s"
-
-  def dumpFileToRedis(filePath: String) = {
-    val redisKey = redisKeyForPath(filePath)
-    val fileToMorphemes = FileMorphemesToRedis(filePath, redisPool, redisKey, dropBlacklisted = dropBlacklisted, onlyWhitelisted = onlyWhitelisted)
-    fileToMorphemes.dumpToRedis
-    redisKey
-  }
 }
---
+++ src/main/scala/org/beachape/actors/MorphemesAnalyzerActor.scala
@@ -0,0 +1,38 @@
+package org.beachape.actors
+
+import akka.actor.Actor
+import akka.event.Logging
+import com.redis._
+import org.beachape.analyze.{Morpheme, RedisHelper}
+
+
+class MorphemesAnalyzerActor(redisPool: RedisClientPool) extends Actor with RedisHelper{
+
+  def receive = {
+
+    case List('dumpMorphemesToRedis, redisKey: RedisKey, line: String, dropBlacklisted:Boolean, onlyWhitelisted: Boolean) => {
+      val morphemes = Morpheme.stringToMorphemes(line, dropBlacklisted, onlyWhitelisted)
+      storeAllInRedis(morphemes, redisKey)
+    }
+
+    case _ => println("huh?")
+  }
+
+  private def storeAllInRedis(morphemeList: List[Morpheme], redisKey: RedisKey) = {
+    for (morpheme <- morphemeList) {
+      storeInRedis(morpheme, redisKey)
+    }
+  }
+
+  private def storeInRedis(morpheme: Morpheme, redisKey: RedisKey) = {
+    redisPool.withClient {
+      redis =>
+        {
+          redis.zincrby(redisKey.redisKey, 1, morpheme.surface)
+          redis.zincrby(redisKey.redisKey, 1, zSetTotalScoreKey)
+        }
+    }
+
+  }
+
+}
