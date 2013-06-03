7554259486364c855a5c48b69e718eefb18f4ffb
Lloyd Meta <lloydmeta@gmail.com>
2013-06-04 08:15:28 +0900

    Alternate

--- src/main/scala/org/beachape/analyze/ChiSquare.scala
+++ src/main/scala/org/beachape/analyze/ChiSquare.scala
@@ -6,23 +6,26 @@ trait ChiSquare {
   def calculateChiSquaredForTerm(oldTermScore: Double, termScore: Double, oldSetTotalScore: Double, newSetTotalScore: Double): Double = {
 
     // Calculate frequencies with add 1 smoothing
-    val observedTermFrequency = termScore / (if (newSetTotalScore > 0) newSetTotalScore else 1)
-    val expectedTermFrequency = adjustedExpectedFrequency(oldTermScore, oldSetTotalScore)
-    val otherObservedFrequency = (newSetTotalScore - termScore) / (if (newSetTotalScore > 0) newSetTotalScore else 1)
-    val otherExpectedFrequency = adjustedExpectedFrequency((oldSetTotalScore - oldTermScore), oldSetTotalScore)
+    val expectedTotalScoreAdjusted = if (oldSetTotalScore > 0) oldSetTotalScore else 1
+    val expectedTermFrequency = oldTermScore / expectedTotalScoreAdjusted
+    val expectedTermFrequencyMin = 1.0 / expectedTotalScoreAdjusted
 
-    val termChiSquaredPart = calculateChiSquaredPart(expectedTermFrequency, observedTermFrequency)
-    val otherChiSquaredPart = calculateChiSquaredPart(otherExpectedFrequency, otherObservedFrequency)
+    val observedTermFrequency = termScore / newSetTotalScore
+
+    val otherExpectedFrequency = (oldSetTotalScore - oldTermScore) / expectedTotalScoreAdjusted
+    val otherExpectedFrequencyMin = 1.0 / expectedTotalScoreAdjusted
+
+    val otherObservedFrequency = (newSetTotalScore - termScore) / newSetTotalScore
+
+    val termChiSquaredPart = calculateChiSquaredPart(expectedTermFrequency, observedTermFrequency, expectedTermFrequencyMin)
+    val otherChiSquaredPart = calculateChiSquaredPart(otherExpectedFrequency, otherObservedFrequency, otherExpectedFrequencyMin)
 
     termChiSquaredPart + otherChiSquaredPart
   }
 
-  private def calculateChiSquaredPart(expectedFrequency: Double, observedFrequency: Double) = {
-    pow(((observedFrequency * 100 - expectedFrequency * 100).abs - 0.5), 2) / (expectedFrequency * 100)
+  private def calculateChiSquaredPart(expectedFrequency: Double, observedFrequency: Double, expectedFrequencyMin: Double) = {
+    pow(((observedFrequency * 100 - expectedFrequency * 100).abs - 0.5), 2) /
+      ((if (expectedFrequency == 0) expectedFrequencyMin else expectedFrequency) * 100)
   }
 
-  private def adjustedExpectedFrequency(termScore: Double, totalScore: Double) = {
-    val frequency = termScore / (if (totalScore > 0) totalScore else 1)
-    if (frequency == 0) (1.0 / totalScore) else frequency
-  }
 }
